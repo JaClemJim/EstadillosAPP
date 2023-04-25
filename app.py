2c541cfe9b23a6eb802621feddce770ce116164a
JaClemJim <jose.clemente@ineco.com>
2023-04-25 16:49:38 +0200

    "APP WEB funcionando"

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
 import streamlit as st
 import pandas as pd
 from shift_scheduling_sat_revCREF_v20 import solve_shift_scheduling
+import base64
+import requests, os
 
-'#ESTADILLOS INTERACTIVOS'
 
-# @st.cache_data
-# def load_data(path):
-#     dataset = pd.read_excel(path, source='openpyxl')
-#     return dataset
+######
+#Entradilla
+######
+st.write(""" # ESTADILLOS INTERACTIVOS """)
 
-estadillo = solve_shift_scheduling(False,False)
+st.markdown("""
+ Seleccione los parámetros: 
+""")
 
-st.dataframe(estadillo)
+######
+# Carga de los resultados en cache
+######
+@st.cache_data
+def load_data(datos):
+    dataset = solve_shift_scheduling(datos)
+    return dataset
+
+aerop = st.text_input("código OACI")
+atcos = st.number_input('Número de ATCOS disponibles para el turno', min_value=1, step=1)
+turno = st.selectbox('0 -> Mañana, 1 -> tarde, 2 -> noche', [0,1,2])
+bloque = st.number_input('Bloque de tiempo para dividir la hora', min_value=5, max_value= 60, step=5)
+demanda = st.number_input('Bloque de tiempo para captar la demanda', min_value=5, max_value = 60, step=5)
+
+
+list_input = [aerop, atcos, turno, bloque, demanda]
+
+boton1 = st.button("Click para calcular")
+
+st.write("boton:", boton1)
+
+if boton1:
+    estadillo = load_data(list_input)
+    
+    st.write(estadillo)
